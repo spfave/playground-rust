ad99973b1a1fd0444c2c2b7abec5f8b524922452
Sebastian F <sebpfdev@gmail.com>
2022-06-14 00:31:08 -0400

    Complete lesson 8

---
+++ msft-learn-rust/demo/src/08-generic-types-traits.rs
@@ -0,0 +1,95 @@
+// LESSON 8.7: Implement a generic type
+fn main() {
+    assert_eq!(Container::new(42).value, 42);
+    assert_eq!(Container::new(3.14).value, 3.14);
+    assert_eq!(Container::new("Foo").value, "Foo");
+    assert_eq!(
+        Container::new(String::from("Bar")).value,
+        String::from("Bar")
+    );
+    assert_eq!(Container::new(true).value, true);
+    assert_eq!(Container::new(-12).value, -12);
+    assert_eq!(Container::new(Some("text")).value, Some("text"));
+}
+
+struct Container<T> {
+    value: T,
+}
+
+impl<T> Container<T> {
+    pub fn new(value: T) -> Self {
+        Container { value }
+    }
+}
+
+// LESSON 8.8: Implement an iterator
+fn main() {
+    let data = vec![4, 1, 1, 2, 1, 3, 3, -2, -2, -2, 5, 5];
+    // groups:               |->|---->|->|->|---->|---------->|--->|
+    let group1 = Groups::new(data);
+    println!("{:?}", &group1);
+    assert_eq!(
+        group1.into_iter().collect::<Vec<Vec<_>>>(),
+        vec![
+            vec![4],
+            vec![1, 1],
+            vec![2],
+            vec![1],
+            vec![3, 3],
+            vec![-2, -2, -2],
+            vec![5, 5],
+        ]
+    );
+
+    // let data2 = vec![1, 2, 2, 1, 1, 2, 2, 3, 4, 4, 3];
+    // groups:                |->|---->|---->|-----|->|---->|->|
+    // assert_eq!(
+    //     Groups::new(data2).into_iter().collect::<Vec<Vec<_>>>(),
+    //     vec![
+    //         vec![1],
+    //         vec![2, 2],
+    //         vec![1, 1],
+    //         vec![2, 2],
+    //         vec![3],
+    //         vec![4, 4],
+    //         vec![3],
+    //     ]
+    // )
+}
+
+#[derive(Debug)]
+struct Groups<T> {
+    inner: Vec<T>,
+}
+
+impl<T> Groups<T> {
+    fn new(inner: Vec<T>) -> Self {
+        Groups { inner }
+    }
+}
+
+impl<T: PartialEq> Iterator for Groups<T> {
+    type Item = Vec<T>;
+
+    // TODO: Write the rest of this implementation.
+    fn next(&mut self) -> Option<Self::Item> {
+        // Once inner vector is empty iteration is donw
+        if self.inner.is_empty() {
+            return None;
+        }
+
+        // get count of matching values
+        let mut count = 1;
+        let val = &self.inner[0];
+        for v in &self.inner[1..] {
+            if v == val {
+                count += 1
+            } else {
+                break;
+            }
+        }
+
+        let items = self.inner.drain(0..count).collect();
+        Some(items)
+    }
+}
